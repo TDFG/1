358bad072d4560f3bec37b13da4382be36a85870
TDFG <14927346+undefined@users.noreply.github.com>
2022-09-19 19:47:10 +0000

    First commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import { LightningElement } from "lwc";
+
+export default class App extends LightningElement {
+   lead = {
+    firstName: "João",
+    lastName: "Mameluco",
+    age: "18",
+    email: "johnny@boy.org",
+    rating: "Hot",
+    company: "Mamelucos Organization",
+    annualRevenue: "R$15.000,00",
+    }
+}
